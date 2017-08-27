055703c413bb2277d170853a7ce2b9750f73edf5
WenYu <wongwy93@gmail.com>
2017-08-27 15:38:42 +0800

    Complete CRUD of CategoriesController and ProductsController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,101 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+     assign_to_parent
+
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update_attributes(category_params)
+      assign_to_parent
+
+      flash[:success] = "Category updated"
+
+      redirect_to @category
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    delete_node_keep_sub_nodes
+
+    @category.reload
+
+    if Category.destroy(@category.id)
+      flash[:success] = "Category deleted"
+
+      redirect_to categories_path
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+  def assign_to_parent
+    category_parent_id = params[:category][:parent_id]
+
+    if !category_parent_id.empty?
+      category_parent = Category.find(category_parent_id)
+
+      @category.move_to_child_of(category_parent)
+    end
+  end
+
+  def delete_node_keep_sub_nodes
+    if @category.child? && !@category.leaf?
+      move_children_to_immediate_parent
+    else
+      move_children_to_root
+    end
+  end
+
+  def move_children_to_immediate_parent
+    node_immediate_children = @category.children
+    node_immediate_parent = @category.parent
+
+    node_immediate_children.each do |child|
+      child.move_to_child_of(node_immediate_parent)
+      node_immediate_parent.reload
+    end
+  end
+
+  def move_children_to_root
+    node_immediate_children = @category.children
+
+    node_immediate_children.each do |child|
+      child.move_to_root
+      child.reload
+    end
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,68 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    find_and_assign_category
+
+    if @product.save
+      flash[:success] = "Product created"
+
+      redirect_to @product
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update_attributes(product_params)
+      find_and_assign_category
+
+      flash[:success] = "Product updated"
+      redirect_to @product
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    if @product.destroy
+      flash[:success] = "Product deleted"
+
+      redirect_to @products_path
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :price
+    )
+  end
+
+  def find_and_assign_category
+    category = Category.find(params[:product][:category])
+    @product.category = category
   end
 end
