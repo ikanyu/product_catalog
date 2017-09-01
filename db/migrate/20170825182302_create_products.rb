b6436d59d967bfa17d5e9fb0ffc28eb066ee9192
WenYu <wongwy93@gmail.com>
2017-09-01 16:25:46 +0800

    Improvements to page

--- db/migrate/20170825182302_create_products.rb
+++ db/migrate/20170825182302_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.decimal :price, precision: 5, scale: 2
+      t.decimal :price, precision: 8, scale: 2
       t.integer :category_id
       t.timestamps
     end
